ec358e81b2a344fd62183d902985928db1507ab3
Donghae <ldonghae320@gmail.com>
2021-08-17 23:21:42 +0900

    210817

---
+++ 그리디/큰 수의법칙.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open("input.txt","r")   
+
+n, m ,k = map(int, input().split())
+data = list(map(int,input().split()))
+data.sort()
+first, second = data[-1], data[-2]
+
+count = (m // (k + 1)) * k + (m % (k + 1))
+res = first * count
+res += second * (m - count)
+print(res)
--- 수학/2775_부녀회장이 될테야.py
+++ 수학/2775_부녀회장이 될테야.py
@@ -1,16 +1,9 @@
-import sys
-sys.stdin = open("input.txt","r")   
-
-def cnt(n, room):
-    temp = room[:n + 1]
-    for i in range(1, n + 1):
-        room[i] = sum(temp[:i + 1])
-    return room
 case = int(input())
 for _ in range(case):
     room = list(range(15))
     k = int(input())
     n = int(input())
     for i in range(k):
-        room = cnt(n, room)
+        for i in range(1, n + 1):
+            room[i] += room[i-1]
     print(room[n])
