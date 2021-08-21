30405d27c234ae5503e844d10a734d675500cb81
Donghae <ldonghae320@gmail.com>
2021-08-21 23:58:55 +0900

    210821

--- 브루트포스/1018_체스판 다시 칠하기.py
+++ 브루트포스/1018_체스판 다시 칠하기.py
@@ -1,3 +1,23 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+n, m = map(int, input().split())
+lst = []
+for _ in range(n):
+    lst.append(input())
+
+res = []
+for i in range(n - 7):
+    for j in range(m - 7):
+        start_W, start_B = 0, 0
+        for k in range(i, i + 8):
+            for l in range(j, j + 8):              
+                if (k + l)%2 == 0:  
+                    if lst[k][l] != 'W': start_W += 1  
+                    if lst[k][l] != 'B': start_B += 1
+                else :
+                    if lst[k][l] != 'B': start_W += 1
+                    if lst[k][l] != 'W': start_B += 1
+        res.append(start_W)                         
+        res.append(start_B)                         
+print(min(res)) 
